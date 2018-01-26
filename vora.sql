5d3d95b03d780c67ec2f2e9a3cc5c8ee316ec4ed
D047102 <alexander.bruch@sap.com>
2018-01-26 17:57:50 +0100

    Friday late

--- vora.sql
+++ vora.sql
@@ -5,8 +5,14 @@ LOAD COLLECTION "VORA"."CAR_TELEMETRY_COLLECTION";
 
 drop table "VORA"."CAR_TELEMETRY_ARCHIVE";
 CREATE TABLE "VORA"."CAR_TELEMETRY_ARCHIVE" ( speed VARCHAR(32) ,battery VARCHAR(32) ,gear VARCHAR(32) ,testcase VARCHAR(32) , timest VARCHAR(64) ,macaddress VARCHAR(64) )  STORE IN MEMORY;
-ALTER TABLE "VORA"."CAR_TELEMETRY_ARCHIVE" ADD DATASOURCE HDFS('hdfs://spark-hdfs-adapter:8020/car_telemetry_solution/CarTelemetryStructuredDataExtract.csv')  DELIMITED BY ',';
+ALTER TABLE "VORA"."CAR_TELEMETRY_ARCHIVE" ADD DATASOURCE HDFS('hdfs://spark-hdfs-adapter:8020/car_telemetrydata/CarTelemetryStructuredDataExtract.csv')  DELIMITED BY ',';
 LOAD TABLE "VORA"."CAR_TELEMETRY_ARCHIVE";
+
+
+drop table "VORA"."CAR_TELEMETRY_TRAINING";
+CREATE TABLE "VORA"."CAR_TELEMETRY_TRAINING" ( speed VARCHAR(32) ,battery VARCHAR(32) ,gear VARCHAR(32) ,testcase VARCHAR(32) , timest VARCHAR(64) ,macaddress VARCHAR(64) )  STORE IN MEMORY;
+ALTER TABLE "VORA"."CAR_TELEMETRY_TRAINING" ADD DATASOURCE HDFS('hdfs://spark-hdfs-adapter:8020/car_telemetry_solution/CarTelemetryStructuredTrainingData.csv')  DELIMITED BY ',';
+LOAD TABLE "VORA"."CAR_TELEMETRY_TRAINING";
 
 
 
